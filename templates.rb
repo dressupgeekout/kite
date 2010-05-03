4f5baa35fe0c8babbc545ab7b6fbea7bc49314d4
Christian Koch <ckoch002@student.ucr.edu>
2010-05-02 17:35:34 -0700

    Added a layout engine and DRYed up the template code

--- templates.rb
+++ templates.rb
@@ -3,12 +3,25 @@ require 'erb'
 class Kite
 
   def erb(template)
+    body = try_to_read(template)
+    puts ERB.new(body).result(binding)
+  end
+
+  def render(template, layout='views/layout.erb', layout_splitter='[[ YIELD ]]')
+    split_layout = File.read(layout).split(layout_splitter)
+    body = try_to_read(template)
+    puts ERB.new(split_layout.first + body + split_layout.last).result(binding)
+  end
+
+  private
+
+  def try_to_read(template)
     begin
       body = File.read(template)
     rescue
       default!
     end
-    puts ERB.new(body).result(binding)
+    body
   end
 
 end
